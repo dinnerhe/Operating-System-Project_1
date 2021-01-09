254a844c69e9d66cc849ac351aa51e1d816f373b
Qiwei He <qiweih1@uci.edu>
2021-01-09 15:18:17 +0800

    Initial Commit

---
+++ HW1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Tony on 2021/1/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
